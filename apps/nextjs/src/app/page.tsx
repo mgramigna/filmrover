5f8051797ec3eda393c5b987d1841fb0897cdf9c
Matt Gramigna <matthewgramigna@gmail.com>
2023-12-27 21:48:45 -0500

    chore: remove example query from homepage

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,10 +1,4 @@
-import { api } from "@/trpc/server";
-
 export default async function HomePage() {
-  const movie = await api.movie.getById.query({
-    id: 11,
-  });
-
   return (
     <main className="dark flex h-screen flex-col items-center bg-gradient-to-b from-slate-800 to-slate-950 text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
