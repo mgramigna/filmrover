ed34494f5c6fe1261d9fbdb3e8707fde6e0dfae0
Matt Gramigna <matthewgramigna@gmail.com>
2023-12-27 22:58:01 -0500

    chore(api): add trpc route for creating a game

--- packages/api/src/router/game.ts
+++ packages/api/src/router/game.ts
@@ -1,3 +1,6 @@
+import { init } from "@paralleldrive/cuid2";
+import { TRPCError } from "@trpc/server";
+
 import { games } from "@filmrover/db";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
@@ -7,5 +10,30 @@ export const gameRouter = createTRPCRouter({
     const result = await ctx.db.select().from(games);
 
     return result;
+  }),
+  createGame: publicProcedure.mutation(async ({ ctx }) => {
+    const createId = init({
+      length: 10,
+    });
+
+    const id = createId();
+
+    const [result] = await ctx.db
+      .insert(games)
+      .values({
+        id,
+      })
+      .returning({
+        gameId: games.id,
+      });
+
+    if (!result) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Error creating game",
+      });
+    }
+
+    return result.gameId;
   }),
 });
