56c08809432bf0fe42ff29c3c05bc36f4e18b021
mgramigna <matthewgramigna@gmail.com>
2025-01-17 14:02:17 -0500

    chore: astro types

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import aws from "astro-sst";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
+  image: {
+    service: passthroughImageService(),
+  },
 });
